ced5512000edbc0dc85ea2ef6eff544cd6c1a321
ZhaoBan1124 <twt_1124@qq.com>
2025-02-26 10:47:39 +0800

    第一次提交

---
+++ exp1_4_1.py
@@ -0,0 +1,13 @@
+print("Hello,world!")
+
+'''
+用于整段注释
+'''
+a=5
+b=6
+if a>b:
+    print('结果为True')
+    print('a大于b')
+else:
+    print('结果为False')
+    print('a小于等于b')
---
+++ exp1_4_2.py
@@ -0,0 +1,9 @@
+str=input('请输入一段文字：')
+print(str)
+print(type(str))
+x=int(input('请输入x的值：'))
+print('x的平方：',x*x)
+
+str1,str2=input('请输入两个学生姓名，用空格隔开：').split()
+print('第一位学生是：',str1)
+print('第二位学生是：',str2)
