31250b3ecf6ebf709fb64aa785daf0bb109c732a
ZhaoBan1124 <twt_1124@qq.com>
2025-02-27 14:29:51 +0800

    第一次上传

--- exp1_4_1__test.py
+++ exp1_4_1__test.py
@@ -1,13 +1,19 @@
-print("Hello,world!")
+from operator import truediv
 
-'''
-用于整段注释
-'''
-a=5
-b=6
-if a>b:
-    print('结果为True')
+print("Hello,world!") # 输出(内容)
+
+''' #表示整段注释(1/2)
+print('不会输出这里的内容')
+''' #表示整段注释(2/2)
+
+a=5 #把变量a赋值为整数(int)5
+b=6.8 #把变量b赋值为浮点数(float)6.8
+print(type(a)) #输出 获取的a对象的数据类型
+print(type(b)) #输出 获取的b对象的数据类型
+
+if a>b: #条件判断(a是否大于b)
+    print('结果为True') #如果结果为True,则执行这里的所有代码
     print('a大于b')
 else:
-    print('结果为False')
+    print('结果为False') #如果结果为False,则执行这里的所有代码
     print('a小于等于b')
