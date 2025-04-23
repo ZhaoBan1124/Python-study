83f52ef2f804b3f8d32af6101845367048a47c59
ZhaoBan1124 <twt_1124@qq.com>
2025-04-23 13:18:44 +0800

    第八次提交

--- exp4_1_7.py
+++ exp4_1_7.py
@@ -1,11 +1,11 @@
 def calculate_total(prices,quantities):
-    print(len(prices))
-    print(len(quantities))
     num=0
-    for i in range(0,len(prices)):
-        num += eval(prices[i]*eval(quantities[i]))
-    return num
 
+    for i in range(0,len(prices)):
+        price = float(prices[i])
+        quantitie=int(quantities[i])
+        num += float(price*quantitie)
+    return num
 prices=input('请输入商品价格(以空格分割):').split()
 quantities=input('请输入商品数量(以空格分割):').split()
-print(calculate_total(prices,quantities))
+print(f"总金额：{calculate_total(prices,quantities)}")
